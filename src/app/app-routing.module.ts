ae5bbdd4d004912e0f669c64e5d010f9067ee873
PC <Lmaodiego@gmail.com>
2021-06-20 18:24:31 -0500

    Merge- Resolved Problem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { ProductComponent } from "./pages/product/product.component";
 import {ProductsComponent} from "./pages/products/products.component";
 import {UserComponent} from "./pages/user/user.component";
 import {EditProfileComponent} from "./pages/edit-profile/edit-profile.component";
+import {HomeComponent} from "./pages/home/home/home.component";
 
 const routes: Routes = [
-
-  //{ path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
   { path: 'products', component: ProductsComponent },
   { path: 'products/new', component: ProductComponent },
   { path: 'products/:id', component: ProductComponent },
