9b0f875c508eb1bab34e3391bbb53a85423f9a14
PC <Lmaodiego@gmail.com>
2021-06-30 22:11:39 -0500

    FIX models/orders

--- src/app/models/order/order.ts
+++ src/app/models/order/order.ts
@@ -1,10 +1,8 @@
 export interface Order {
   id: number;
-  date: string;
-  order_amount: number;
-  //
-  // amount : number;
-  // purchase_date: string;
+  userid: number;
+  amount : number;
+  purchase_date: string;
   delivery_date : string;
   delivery_address: string;
 }
